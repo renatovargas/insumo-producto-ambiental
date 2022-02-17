f15df44944eaeb78b3f3b7f8e9a878c34b543957
Renato Vargas <renatovargas@users.noreply.github.com>
2022-02-17 08:52:29 -0600

    Cuenta de Energía

--- codigo/modulo_03.R
+++ codigo/modulo_03.R
@@ -9,6 +9,7 @@
 library(openxlsx)
 library(reshape2)
 library(Matrix.utils)
+library(ggplot2)
 rm(  list = ls()  )
 
 
@@ -35,6 +36,7 @@ nombres <- read.xlsx("MIP-AE-AE-017-CR.xlsx",
                                colNames = FALSE, 
                                rowNames = FALSE
 )  # <-- Fin del paréntesis
+
 
 # Agregamos nuestra matriz para tener un valor por actividad
 
@@ -82,7 +84,7 @@ rownames(DF_cruda) <- as.vector(rownames(Z_cruda))
 DF <- aggregate.Matrix(DF_cruda, as.factor(rownames(DF_cruda)),fun = "sum")
 DF <- as.matrix(DF)
 
-dim(Z)
+dim(DF)
 
 
 
@@ -272,7 +274,239 @@ E1 <- EC %*% diag(as.vector(L %*% f1))
 colnames(E1) <- colnames(E)
 
 # =============================================================================
+# Bioeconomía
+
+# Clasificación cruzada bioeconomía AECR
+nombres_bio <- read.xlsx("clasificacion_cruzada_bioeconomia.xlsx", 
+                                            sheet = "datos", 
+                                            rows= c(1:137), 
+                                            cols = c(1:6), 
+                                            skipEmptyRows = FALSE, 
+                                            colNames =TRUE, 
+                                            rowNames = FALSE
+)  # <-- Fin del paréntesis
+
+# Primero solamente emitir los multiplicadores de los sectores
+rownames(L) <- colnames(L)
+bio_mult <- as.data.frame(colSums(L))
+
+# Lo copiamos para pegarlo
+write.table(as.data.frame(colSums(L)),"clipboard", sep = "\t", row.names = TRUE)
+
+
+# Luego agregamos nuestras matrices para considerar a todas las actividades
+# características como un solo sector
+
+Z_bio <- Z
+
+# Renombramos con la clasificación cruzada
+colnames(Z_bio) <- nombres_bio$BECR
+rownames(Z_bio) <- nombres_bio$BECR
+
+# Y Agregamos
+Z_bio <- aggregate.Matrix(Z_bio, as.factor(nombres_bio$BECR),fun = "sum")
+Z_bio <- t(aggregate.Matrix(t(Z_bio), as.factor(nombres_bio$BECR),fun = "sum"))
+Z_bio <- as.matrix(Z_bio)
+
+# Repetimos  para la demanda final
+
+f_bio <- as.matrix(f)
+rownames(f_bio) <- nombres_bio$BECR
+
+f_bio <- aggregate.Matrix(f_bio, as.factor(nombres_bio$BECR),fun = "sum")
+f_bio <- as.matrix(f_bio)
+f_bio <- as.vector(f_bio)
+
+# Modelo insumo producto
+
+# Modelo de insumo producto
+
+# Producción
+x_bio <- as.vector(rowSums(Z_bio) + f_bio)
+
+# Demanda final
+f_bio <- f_bio
+
+# x sombrero
+xhat_bio <- diag(x_bio)
+xhat_inv_bio <- solve(xhat_bio)
+
+# Matriz de coeficientes técnicos
+A_bio <- Z_bio %*% solve( xhat_bio )
+
+# Matriz identidad
+I_bio <- diag( dim(A_bio)[1])
+
+# Matriz de Leontief
+L_bio <- solve(I_bio - A_bio )
+
+bio_mult2 <- as.data.frame(colSums(L_bio))
+
+write.table(bio_mult2,"clipboard", sep = "\t", row.names = TRUE)
+
+#
+
+
+# Empleo BIO
+# ==========
+
+# Consumo intermedio
+LAB_cruda <- as.matrix(read.xlsx("MIP-AE-AE-017-CR.xlsx", 
+                               sheet = "MIP 2017", 
+                               rows= c(296:301), 
+                               cols = c(4:256), 
+                               skipEmptyRows = FALSE, 
+                               colNames = FALSE, 
+                               rowNames = FALSE)
+)  # <-- Fin del paréntesis
+
+nombres_l <- read.xlsx("MIP-AE-AE-017-CR.xlsx", 
+                     sheet = "MIP 2017", 
+                     rows= c(296:301), 
+                     cols = 1, 
+                     skipEmptyRows = FALSE, 
+                     colNames = FALSE, 
+                     rowNames = FALSE
+)  # <-- Fin del paréntesis
+
+
+# Agregamos nuestra matriz para tener un valor por actividad
+
+# Nombramos nuestra matriz Z
+colnames(LAB_cruda) <- as.vector(nombres$X1)
+rownames(LAB_cruda) <- as.vector(nombres_l$X1)
+
+LAB <- aggregate.Matrix(LAB_cruda, as.factor(nombres_l$X1),fun = "sum")
+LAB <- t(aggregate.Matrix(t(LAB), as.factor(nombres$X1),fun = "sum"))
+LAB <- as.matrix(LAB)
+rm(LAB_cruda)
+
+dim(LAB)
+write.table(multip_cp,"clipboard", sep = "\t", row.names = TRUE)
+# Multiplicadores de empleo
+# Todos
+
+c_total     <- colSums(LAB) %*% xhat_inv
+c_total_hat <- diag(as.vector(c_total))
+H_total <- c_total_hat %*% L
+multip_total <- as.matrix(colSums(H_total))
+
+# Asalariados
+
+c_asal     <- LAB[1,] %*% xhat_inv
+c_asal_hat <- diag(as.vector(c_asal))
+H_asal <- c_asal_hat %*% L
+multip_asal <- as.matrix(colSums(H_asal))
+
+# Cuenta propia
+
+c_cp     <- LAB[2,] %*% xhat_inv
+c_cp_hat <- diag(as.vector(c_cp))
+H_cp <- c_cp_hat %*% L
+multip_cp <- as.matrix(colSums(H_cp))
+
+# Empresarios
+
+c_empr     <- LAB[3,] %*% xhat_inv
+c_empr_hat <- diag(as.vector(c_empr))
+H_empr <- c_empr_hat %*% L
+multip_empr <- as.matrix(colSums(H_empr))
+
+# Trabajadores familiares no remunerados
+
+c_tfnr     <- LAB[4,] %*% xhat_inv
+c_tfnr_hat <- diag(as.vector(c_tfnr))
+H_tfnr <- c_tfnr_hat %*% L
+multip_tfnr <- as.matrix(colSums(H_tfnr))
+
+# Otros trabajadores no remunerados
+
+c_otnr     <- LAB[5,] %*% xhat_inv
+c_otnr_hat <- diag(as.vector(c_otnr))
+H_otnr <- c_otnr_hat %*% L
+multip_otnr <- as.matrix(colSums(H_otnr))
+
+# Personal de otros establecimientos
+
+c_otestab     <- LAB[6,] %*% xhat_inv
+c_otestab_hat <- diag(as.vector(c_otestab))
+H_otestab <- c_otestab_hat %*% L
+multip_otestab <- as.matrix(colSums(H_otestab))
+
+# Multiplicadores BIO
+LAB_bio <- LAB
+colnames(LAB_bio) <- nombres_bio$BECR
+
+# Y Agregamos
+LAB_bio <- aggregate.Matrix(LAB_bio, as.factor(rownames(LAB_bio)),fun = "sum")
+LAB_bio <- t(aggregate.Matrix(t(LAB_bio), as.factor(nombres_bio$BECR),fun = "sum"))
+LAB_bio <- as.matrix(LAB_bio)
+
+
+
+# Multiplicadores de empleo BIO
+# Todos
+
+c_total_bio     <- colSums(LAB_bio) %*% xhat_inv_bio
+c_total_hat_bio <- diag(as.vector(c_total_bio))
+H_total_bio <- c_total_hat_bio %*% L_bio
+multip_total_bio <- as.matrix(colSums(H_total_bio))
+
+# Asalariados
+
+c_asal_bio     <- LAB_bio[1,] %*% xhat_inv_bio
+c_asal_hat_bio <- diag(as.vector(c_asal_bio))
+H_asal_bio <- c_asal_hat_bio %*% L_bio
+multip_asal_bio <- as.matrix(colSums(H_asal_bio))
+
+# Cuenta propia
+
+c_cp_bio     <- LAB_bio[2,] %*% xhat_inv_bio
+c_cp_hat_bio <- diag(as.vector(c_cp_bio))
+H_cp_bio <- c_cp_hat_bio %*% L_bio
+multip_cp_bio <- as.matrix(colSums(H_cp_bio))
+
+# Empresarios
+
+c_empr_bio     <- LAB_bio[3,] %*% xhat_inv_bio
+c_empr_hat_bio <- diag(as.vector(c_empr_bio))
+H_empr_bio <- c_empr_hat_bio %*% L_bio
+multip_empr_bio <- as.matrix(colSums(H_empr_bio))
+
+# Trabajadores familiares no remunerados
+
+c_tfnr_bio     <- LAB_bio[4,] %*% xhat_inv_bio
+c_tfnr_hat_bio <- diag(as.vector(c_tfnr_bio))
+H_tfnr_bio <- c_tfnr_hat_bio %*% L_bio
+multip_tfnr_bio <- as.matrix(colSums(H_tfnr_bio))
+
+# Otros trabajadores no remunerados
+
+c_otnr_bio     <- LAB_bio[5,] %*% xhat_inv_bio
+c_otnr_hat_bio <- diag(as.vector(c_otnr_bio))
+H_otnr_bio <- c_otnr_hat_bio %*% L_bio
+multip_otnr_bio <- as.matrix(colSums(H_otnr_bio))
+
+# Personal de otros establecimientos
+
+c_otestab_bio     <- LAB_bio[6,] %*% xhat_inv_bio
+c_otestab_hat_bio <- diag(as.vector(c_otestab_bio))
+H_otestab_bio <- c_otestab_hat_bio %*% L_bio
+multip_otestab_bio <- as.matrix(colSums(H_otestab_bio))
+
+
+# =============================================================================
 # Excel
+
+write.xlsx(as.data.frame(colnames(Z)),
+                         "nombres_Z.xlsx",
+                         sheetName = "datos",
+                         startRow = 5,
+                         startCol = 1,
+                         asTable = FALSE,
+                         colNames = TRUE,
+                         rowNames = TRUE,
+                         overwrite = TRUE)
 
 # Cambios en datos ambientales
 write.xlsx( as.data.frame(deltaE) , 
@@ -289,60 +523,60 @@ write.xlsx( as.data.frame(deltaE) ,
 # Gráfico
 heatmap(E1, Colv = NA, Rowv = NA)
 
-# =============================================================================
-# Bloopers
-
-# Inicialmente creímos que la matriz Z tenía un componente importado
-# y un componente nacional. Ese no es el caso, solo está dividida por
-# el tipo de control "nacional" o "extranjero" de la producción nacional.
-# Gracias a Johnny Aguilar por la aclaración.
-
-# Dejo la solución para extraer filas y columnas con índices, solamente
-# porque es una buena ilustración de algo que se utiliza continuamente
-# en el trabajo con este tipo de datos.
-
-# Obtenemos nuestros códigos de actividad y nombres
-cods <- as.data.frame(
-  read.xlsx(
-    "MIP-AE-AE-017-CR.xlsx",
-    sheet = "clasificaciones", 
-    rows= c(5:141), 
-    cols = c(1:5), 
-    skipEmptyRows = FALSE, 
-    colNames = TRUE, 
-    rowNames = FALSE
-  )
-)# <-- Fin del paréntesis
-
-# componente doméstico
-local <- c(2,4,6,8,10,12,14,16,18,20,22,24,26,28,30, 
-           32,34,36,38,40,42,44,46,48,50,52,54,56,58,60, 
-           62,64,66,68,70,71,73,75,77,78,80,81,83,85,87,88, 
-           90,92,94,95,97,98,99,100,101,103,105,107,108,110, 
-           112,114,115,117,119,120,121,123,125,127,129,131, 
-           133,135,137,139,141,143,145,147,149,151,153,155, 
-           157,159,161,162,164,166,167,169,171,173,174,176, 
-           178,180,182,184,186,188,190,192,194,196,197,199, 
-           200,202,204,206,208,210,212,214,216,218,219,221, 
-           223,225,227,229,231,233,235,237,239,241,243,245, 
-           247,249,251,253)
-
-# Si el BCR hubiera dejado (aunque en ceros) el componente importado
-# de los sectores que solamente tienen componente local, los indices
-# serían más fáciles de construir con:
-# seq( from=2, to= 254, by=2 ) para lo doméstico 
-
-# Aquí utilizamos el método del índice en R para obtener nuestros elementos
-
-# Compras entre actividades económicas domésticas (consumo intermedio)
-Z <- Z_cruda[  local  ,  local   ]
-
-# Compras de producto importado de las actividades domésticas (importaciones)
-M <- Z_cruda[-local,local]
-
-# Compras de Actividades económicas no-domésticas a las actividades locales
-# (exportaciones)
-X <- Z_cruda[local,-local]
-
-# Compras de Actividades no-domésticas a no-domésticas?
-XX <- Z_cruda[-local,-local]
+# # =============================================================================
+# # Bloopers
+# 
+# # Inicialmente creímos que la matriz Z tenía un componente importado
+# # y un componente nacional. Ese no es el caso, solo está dividida por
+# # el tipo de control "nacional" o "extranjero" de la producción nacional.
+# # Gracias a Johnny Aguilar por la aclaración.
+# 
+# # Dejo la solución para extraer filas y columnas con índices, solamente
+# # porque es una buena ilustración de algo que se utiliza continuamente
+# # en el trabajo con este tipo de datos.
+# 
+# # Obtenemos nuestros códigos de actividad y nombres
+# cods <- as.data.frame(
+#   read.xlsx(
+#     "MIP-AE-AE-017-CR.xlsx",
+#     sheet = "clasificaciones", 
+#     rows= c(5:141), 
+#     cols = c(1:5), 
+#     skipEmptyRows = FALSE, 
+#     colNames = TRUE, 
+#     rowNames = FALSE
+#   )
+# )# <-- Fin del paréntesis
+# 
+# # componente doméstico
+# local <- c(2,4,6,8,10,12,14,16,18,20,22,24,26,28,30, 
+#            32,34,36,38,40,42,44,46,48,50,52,54,56,58,60, 
+#            62,64,66,68,70,71,73,75,77,78,80,81,83,85,87,88, 
+#            90,92,94,95,97,98,99,100,101,103,105,107,108,110, 
+#            112,114,115,117,119,120,121,123,125,127,129,131, 
+#            133,135,137,139,141,143,145,147,149,151,153,155, 
+#            157,159,161,162,164,166,167,169,171,173,174,176, 
+#            178,180,182,184,186,188,190,192,194,196,197,199, 
+#            200,202,204,206,208,210,212,214,216,218,219,221, 
+#            223,225,227,229,231,233,235,237,239,241,243,245, 
+#            247,249,251,253)
+# 
+# # Si el BCR hubiera dejado (aunque en ceros) el componente importado
+# # de los sectores que solamente tienen componente local, los indices
+# # serían más fáciles de construir con:
+# # seq( from=2, to= 254, by=2 ) para lo doméstico 
+# 
+# # Aquí utilizamos el método del índice en R para obtener nuestros elementos
+# 
+# # Compras entre actividades económicas domésticas (consumo intermedio)
+# Z <- Z_cruda[  local  ,  local   ]
+# 
+# # Compras de producto importado de las actividades domésticas (importaciones)
+# M <- Z_cruda[-local,local]
+# 
+# # Compras de Actividades económicas no-domésticas a las actividades locales
+# # (exportaciones)
+# X <- Z_cruda[local,-local]
+# 
+# # Compras de Actividades no-domésticas a no-domésticas?
+# XX <- Z_cruda[-local,-local]
